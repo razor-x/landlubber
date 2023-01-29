0bd4b9e917ae78114590bf32de5acce10c45885f
Evan Sosenko <razorx@evansosenko.com>
2023-01-28 20:58:58 -0800

    Add argv option

--- src/lib/landlubber.ts
+++ src/lib/landlubber.ts
@@ -7,17 +7,21 @@ import { type DefaultContext, defaultMiddleware } from 'lib/context.js'
 
 export interface LandlubberOptions {
   middleware?: MiddlewareFunction[]
+  argv?: readonly string[]
 }
 
 export const landlubber = <Context = DefaultContext>(
   commands: Array<CommandModule<Context>> = [],
-  { middleware = defaultMiddleware }: LandlubberOptions = {}
+  {
+    middleware = defaultMiddleware,
+    argv = process.argv
+  }: LandlubberOptions = {}
 ): Argv => {
-  const argv = yargs(process.argv.slice(2)).middleware(middleware)
+  const builder = yargs(argv.slice(2)).middleware(middleware)
 
   // UPSTREAM: Array argument overload type for yargs.command not implemented.
   // https://github.com/yargs/yargs/issues/2211
-  for (const command of commands) argv.command(command)
+  for (const command of commands) builder.command(command)
 
-  return argv.demandCommand(1, 1).recommendCommands().strict()
+  return builder.demandCommand(1, 1).recommendCommands().strict()
 }
